96874d338713dc2998a8cbe29c6d4b4ed56215f9
unknown <afro123737@gmail.com>
2020-06-08 22:42:39 +0500

    dont with card

--- js/index.js
+++ js/index.js
@@ -16,13 +16,26 @@ window.addEventListener('load', () => {
             prevEl: '.swiper-button-prev',
         },
     });
-})
 
-// active navbar 
-const currentLocation = location.href;
-const links = document.querySelectorAll('.nav-link');
-Array.from(links).forEach(link => {
-    link.href === currentLocation ?
-        link.classList.add('link-active') :
-        link.classList.remove('link-active')
-});
+
+    // active navbar 
+    const currentLocation = location.href;
+    const links = document.querySelectorAll('.nav-link');
+    links.forEach(link => {
+        link.href === currentLocation ?
+            link.classList.add('link-active') :
+            link.classList.remove('link-active')
+    });
+
+    // animations
+    const img = document.querySelectorAll('.display-img');
+
+    img.forEach(el => {
+        el.addEventListener('mouseenter', e => {
+            if (!e.target.classList.contains('slide_img_right'))
+                e.target.classList.add('slide_img_right');
+            e.target.previousElementSibling.classList.add('slide_img_left');
+        })
+    })
+
+})
